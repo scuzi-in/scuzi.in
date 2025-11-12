60c9ffb76951c46a2f4d994748bc912efd9714e1
scuzi-in <info.scuzi@gmail.com>
2025-11-12 10:36:02 +0000

    changes done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "../config/db.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
